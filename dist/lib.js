96c224b82370e9bc0a91bd58ae3f4e82793570e0
Emilia Friedberg <emiliafriedberg@gmail.com>
2017-10-27 11:36:22 -0400

    get lib files

---
+++ dist/lib.js
@@ -0,0 +1,21 @@
+/*!
+ * {LIB} v0.0.2
+ * (c) 2017 {NAME}
+ * Released under the MIT License.
+ */
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(factory((global.lib = global.lib || {})));
+}(this, (function (exports) { 'use strict';
+
+function stringNormalize(string) {
+    // in order: double quotation marks, single quotation marks and apostrophes, commas
+    return string.replace(/[\u201C\u201D\u02DD]/g, '\u0022').replace(/[\u00B4\u0060\u2018\u2019\u0301\u02BB\u02C8]/g, '\u0027').replace(/[\u02CC\u201A\uFF0C]/g, '\u002C');
+}
+
+exports.stringNormalize = stringNormalize;
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
