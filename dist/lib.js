011839d4f6ccb275b55545bc8a995aea5e606a9e
Emilia Friedberg <emiliafriedberg@gmail.com>
2019-01-15 10:25:02 -0500

    rollup

--- dist/lib.js
+++ dist/lib.js
@@ -1,6 +1,6 @@
 /*!
- * {LIB} v0.0.6
- * (c) 2018 {NAME}
+ * {LIB} v0.0.8
+ * (c) 2019 {NAME}
  * Released under the MIT License.
  */
 'use strict';
@@ -9,7 +9,7 @@ Object.defineProperty(exports, '__esModule', { value: true });
 
 function stringNormalize(string) {
     // in order: double quotation marks, single quotation marks and apostrophes, commas
-    return string.replace(/[\u201C\u201D\u02DD]/g, '\u0022').replace(/[\u00B4\u0060\u2018\u2019\u0301\u02BB\u02C8]/g, '\u0027').replace(/[\u02CC\u201A\uFF0C]/g, '\u002C');
+    return string.replace(/[\u201C\u201D\u02DD\u0308¨]/g, '\u0022').replace(/[\u00B4\u0060\u2018\u2019\u0301\u02BB\u02C8]/g, '\u0027').replace(/[\u02CC\u201A\uFF0C、]/g, '\u002C');
 }
 
 exports.stringNormalize = stringNormalize;
